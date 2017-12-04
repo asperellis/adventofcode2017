4258f482e909377f7d4994a3e39af9a7892dc198
Alex Sperellis <asperellis@gmail.com>
2017-12-04 08:54:58 -0500

    day 4 solutions

---
+++ day4.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('pre').innerText.trim().split('\n');
+
+// takes a string of words separated by spaces and returns true only if the string has all unique words
+const hasUniqueWords = (phrase) => {
+  const phraseArr = phrase.trim().split(' ');
+  // make sure array doesn't have any indexes with duplicate values
+  return phraseArr.filter((word, i, arr) => arr.indexOf(word) === i).length === phraseArr.length;
+};
+
+// takes a string of words separated by spaces and returns true only if the string has words that are anagrams of another word in the string
+const anagramCheck = (phrase) => {
+  const phraseArr = phrase.trim().split(' ');
+  let hasAnagrams = false;
+  
+  while(phraseArr.length) {
+    const word = phraseArr.pop().split('').sort().join('');
+    if(phraseArr.filter((w) => w.length === word.length && w.split('').sort().join('') === word).length) {
+      hasAnagrams = true;
+      break;
+    }
+  }
+
+  return hasAnagrams;
+};
+
+// part 1 solution
+console.log(input.filter((passPhrase) => hasUniqueWords(passPhrase)).length);
+
+// part 2 solution
+console.log(input.filter((passPhrase) => hasUniqueWords(passPhrase) && !anagramCheck(passPhrase)).length);
