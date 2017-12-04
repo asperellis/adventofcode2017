8372534f0364808c6bc8deeaa154d9b583834476
Alex Sperellis <asperellis@gmail.com>
2017-12-03 22:20:37 -0500

    day 3

--- day3.js
+++ day3.js
@@ -1,27 +1,85 @@
-function position(n) {
-    const k = Math.ceil((Math.sqrt(n) - 1) / 2);
-    let t = 2 * k + 1;
-    let m = Math.pow(t, 2);
+const gridNumber = 361527;
 
-    t -= 1;
+// from math on Manhattan Disctance Equation
+const calcDistance = (coord1, coord2) => {
+  return Math.abs(coord1.x - coord2.x) + Math.abs(coord1.y - coord2.y);
+};
 
-    if (n >= m - t) {
-        return [k - (m - n), -k];
-    }
+// Part 1 Math From Outside Sources to get the coordinate location of a given int on a spiral grid. Added for note taking
+const getGridCoordinates = (n) => {
+  const k = Math.ceil((Math.sqrt(n) - 1) / 2);
+  let t = 2 * k + 1;
+  let m = Math.pow(t, 2);
 
-    m -= t;
+  t -= 1;
 
-    if (n >= m - t) {
-        return [-k, -k + (m - n)];
-    }
+  if (n >= m - t) {
+    return {
+      x: k - (m - n),
+      y: -k
+    };
+  }
 
-    m -= t;
+  m -= t;
 
-    if (n >= m - t) {
-        return [-k + (m - n), k];
-    }
+  if (n >= m - t) {
+    return {
+      x: -k,
+      y: -k + (m - n)
+    };
+  }
 
-    return [k, k - (m - n - t)];
+  m -= t;
+
+  if (n >= m - t) {
+    return {
+      x: -k + (m - n),
+      y: k
+    };
+  }
+
+  return {
+    x: k,
+    y: k - (m - n - t)
+  };
 }
 
-position(361527)
+// Part 2 Solution From Outside Sources - Added for note taking - user built the actual grid
+const findValueAfter = (n) => {
+  let x = 0;
+  let y = 0;
+  let matrix = {};
+  matrix[x + ',' + y] = 1;
+
+  while(true) {
+      const val = getValue(matrix, x, y);
+      if (val >= n) {
+          return val;
+          break;
+      }
+
+      matrix[x + ',' + y] = val;
+
+      if ((x !== y || x >= 0) && Math.abs(x) <= Math.abs(y)) {
+          x += y >= 0 ? 1 : -1;
+      } else {
+          y += x >= 0 ? -1 : 1;
+      }
+  }
+};
+
+const getValue = (matrix, posX, posY) => {
+  let sum = 0;
+  for (let x = posX - 1; x <= posX + 1; x++) {
+      for(let y = posY - 1; y <= posY + 1; y++) {
+          if (matrix[x + ',' + y]) {
+              sum += matrix[x + ',' + y];
+          }
+      }
+  }
+  return sum;
+}
+
+// Answers
+console.log(calcDistance({ x: 0, y: 0 }, getGridCoordinates(361527)));
+console.log(findValueAfter(361527));
