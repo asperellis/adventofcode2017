6f0b169523b3a7c824389938943a896b6a260743
Alex Sperellis <asperellis@gmail.com>
2017-12-05 10:45:47 -0500

    day 5

--- day5.js
+++ day5.js
@@ -2,16 +2,17 @@ const input = document.body.innerText.trim().split('\n').map((s) => parseInt(s, 
 
 // takes array (maze) and a param to decide whether to increment past spaces by 1 or increase and decrease by one based on its value being >= 3
 const stepsToExit = (maze, incAndDec = false) => {
+  const mazeClone = [...maze];
   let location = 0;
   let steps = 0;
 
   // while the location is inside the maze bounds
-  while(location >= 0 && location < maze.length) {
-    const stepsToTake = maze[location];
+  while(location >= 0 && location < mazeClone.length) {
+    const stepsToTake = mazeClone[location];
     if(stepsToTake >= 3 && incAndDec) {
-      maze[location] = maze[location] - 1;
+      mazeClone[location] = mazeClone[location] - 1;
     } else {
-      maze[location] = maze[location] + 1;
+      mazeClone[location] = mazeClone[location] + 1;
     }
     location += stepsToTake;
     steps++;
@@ -23,5 +24,5 @@ const stepsToExit = (maze, incAndDec = false) => {
 // part 1
 console.log(stepsToExit(input));
 
-// part 2
+// part 2 - just used second input so running in console would work.
 console.log(stepsToExit(input, true));
