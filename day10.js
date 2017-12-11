29f251b4026661ad1303c670d76451a702d890f2
Alex Sperellis <asperellis@gmail.com>
2017-12-11 17:09:34 -0500

    finish day 10

--- day10.js
+++ day10.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
 
 fs.readFile('inputs/day10.txt', 'utf8', (err, data) => {
+  // make a str an ascii code
   const makeAscii = (str) => {
     return str.charCodeAt(0);
   };
 
+  // for part 2
+  const dataAsAscii = data.trim().split('').map(makeAscii);
+  dataAsAscii.push(17, 31, 73, 47, 23);
+
+  // knot hash, takes array of ints and optional repititions param to specify how many times to run initial hashing
   const knotHash = (input, repititions) => {
-    // list 0-255
     const list = [...Array(256).keys()];
     // current position
     let currentPos = 0;
     let skipSize = 0;
     // puzzle input
     let inputLengths = input;
+
+    // make dense hash - for every 16 indexes in array - reduce to a XOR bitwise
     const makeDenseHash = (arr) => {
       const dense = [];
       for(let i = 0; i < arr.length; i+=16) {
@@ -21,6 +28,7 @@ fs.readFile('inputs/day10.txt', 'utf8', (err, data) => {
       return dense;
     };
 
+    // take each XOR / bitwise value and make it a 2 char hex character
     const makeHexHash = (arr) => {
       return arr.reduce((a, b) => {
         let hex = b.toString(16);
@@ -31,22 +39,25 @@ fs.readFile('inputs/day10.txt', 'utf8', (err, data) => {
       }, '');
     }
 
-    const runHash = (rep) => {
+    // function to run one hash
+    const runHash = () => {
       inputLengths.forEach((length, indx) => {
         // select the indexes in the list from currentPos to inputLength and reverse them
         // going beyond the list lenght results in wrapping back to start index
-        let selectedReversed = list.slice(currentPos, currentPos + length).reverse();
+        let selectedReversed = [];
         if(currentPos + length > list.length) {
-          selectedReversed = [...list.slice(currentPos, list.length - 1), ...list.slice(0, length - (list.length - 1 - currentPos))].reverse();
+          selectedReversed = [...list.slice(currentPos, list.length), ...list.slice(0, length - 1 - (list.length - 1 - currentPos))].reverse();
+        } else {
+          selectedReversed = list.slice(currentPos, currentPos + length).reverse();
         }
-        const cap = selectedReversed.length;
-        for(let i = currentPos; i < currentPos + cap; i++) {
-          if(i >= list.length) {
-            list[i % list.length] = selectedReversed.shift();
-          } else {
-            list[i] = selectedReversed.shift();
-          }
+
+        // update the indexes
+        for(let i = currentPos; i < currentPos + length; i++) {
+          const update = selectedReversed.shift();
+          const index = i >= list.length ? i % list.length : i;
+          list[index] = update;
         }
+
         // inc currentPos forward by length plus skip size (currentPos += length + skipSize)
         currentPos += length + skipSize;
         // wrap currentPos around if past list length
@@ -59,22 +70,23 @@ fs.readFile('inputs/day10.txt', 'utf8', (err, data) => {
     };
 
     if(!repititions) {
-      // do once
+      // do once if no repititions declared
       runHash();
       return list[0] * list[1];
     }
 
-    // do a bunch of times
+    // hash numerous times based on repititions parameter
     for (let i = 0; i < repititions; i++) {
-      runHash(i);
+      runHash();
     }
-
+    
+    // take the resulting list and make a dense hash then hex hash from the dense. always returns 32 char string if correct
     return makeHexHash(makeDenseHash(list));
   };
 
+  // part 1
   console.log(knotHash(data.split(',').map(Number)));
-
-  const dataAsAscii = data.trim().split('').map(makeAscii);
-  dataAsAscii.push(17, 31, 73, 47, 23)
+  
+  // part 2
   console.log(knotHash(dataAsAscii, 64));
 });
